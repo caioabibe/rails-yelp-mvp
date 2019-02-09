2e6be15fb77d4cb5b02d38c378c7ea200fc5d483
Caio Abibe <caio.abibe@gmail.com>
2019-02-09 16:48:39 +0800

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Resto 1',
+    address:      'Address 1',
+    category:     'french'
+  },
+  {
+    name:         'Resto 2',
+    address:      'Address 2',
+    category:     'chinese'
+  },
+  {
+    name:         'Resto 3',
+    address:      'Address 3',
+    category:     'italian'
+  },
+  {
+    name:         'Resto 4',
+    address:      'Address 4',
+    category:     'japanese'
+  },
+  {
+    name:         'Resto 5',
+    address:      'Address 5',
+    category:     'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished restaurants!'
+
+puts 'Cleaning database...'
+Review.destroy_all
+
+puts 'Creating reviews...'
+reviews_attributes = [
+  {
+    content:       'Test 1',
+    rating:        5,
+    restaurant_id: 16
+  },
+  {
+    content:       'Test 2',
+    rating:        4,
+    restaurant_id: 16
+  },
+]
+Review.create!(reviews_attributes)
+puts 'Finished!'
